1ce4f8883518dd0ae8b21cc6b6e07cde4f6f828e
Rodrigo Simão <rodrigoip435@gmail.com>
2024-12-15 17:14:13 -0300

    ok, falta implementar os outros projetos

--- javascript/script.js
+++ javascript/script.js
@@ -15,38 +15,6 @@ document.addEventListener('scroll', () => {
     });
 });
 
-// Botão de "Voltar ao Topo"
-const backToTopButton = document.createElement('button');
-backToTopButton.textContent = '⬆️';
-backToTopButton.id = 'back-to-top';
-backToTopButton.style.cssText = `
-    position: fixed;
-    bottom: 20px;
-    right: 20px;
-    padding: 10px 15px;
-    font-size: 1.5rem;
-    background-color: #FF9800;
-    color: #fff;
-    border: none;
-    border-radius: 50%;
-    cursor: pointer;
-    display: none;
-    box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
-`;
-document.body.appendChild(backToTopButton);
-
-backToTopButton.addEventListener('click', () => {
-    window.scrollTo({ top: 0, behavior: 'smooth' });
-});
-
-window.addEventListener('scroll', () => {
-    if (window.scrollY > 300) {
-        backToTopButton.style.display = 'block';
-    } else {
-        backToTopButton.style.display = 'none';
-    }
-});
-
 // Animação suave ao clicar nos links do menu
 document.querySelectorAll('.menu a').forEach((link) => {
     link.addEventListener('click', (event) => {
@@ -54,37 +22,7 @@ document.querySelectorAll('.menu a').forEach((link) => {
         const targetId = link.getAttribute('href').slice(1);
         const targetElement = document.getElementById(targetId);
         if (targetElement) {
-            targetElement.scrollIntoView({ behavior: 'smooth' });
+            targetElement.scrollIntoView({behavior: 'smooth'});
         }
     });
-});
-
-// Carrossel
-const slides = document.querySelectorAll('.slide');
-const prevButton = document.querySelector('.prev');
-const nextButton = document.querySelector('.next');
-let currentSlide = 0;
-
-function updateCarrossel(index) {
-    slides.forEach((slide, i) => {
-        slide.classList.toggle('active', i === index);
-    });
-}
-
-prevButton.addEventListener('click', () => {
-    currentSlide = (currentSlide === 0) ? slides.length - 1 : currentSlide - 1;
-    updateCarrossel(currentSlide);
-});
-
-nextButton.addEventListener('click', () => {
-    currentSlide = (currentSlide === slides.length - 1) ? 0 : currentSlide + 1;
-    updateCarrossel(currentSlide);
-});
-
-// Alternar a classe "active" no menu quando o botão hambúrguer for clicado
-const hamburgerButton = document.querySelector('.hamburger');
-const menu = document.querySelector('.menu');
-
-hamburgerButton.addEventListener('click', () => {
-    menu.classList.toggle('active');
 });
